94de054793d87d53ab27c6beee8b449f0242b756
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-08-25 08:32:22 +0300

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        last_item = items.pop()
+        joined_items = ", ".join(items)
+        return f"{joined_items}, and {last_item}"
